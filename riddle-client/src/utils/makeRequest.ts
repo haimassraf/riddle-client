df93ba4d00a303b2004ceeb0f33f86524bdb197b
Haim Assraf <haimassraf@gmail.com>
2025-09-03 17:43:16 +0300

    chang the URL to Render-URL

--- riddle-client/src/utils/makeRequest.ts
+++ riddle-client/src/utils/makeRequest.ts
@@ -10,7 +10,7 @@ export default async function makeRequest(url: string, method: string = 'GET', b
 
         await new Promise(resolve => setTimeout(resolve, 1000))
 
-        const res = await fetch(`http://localhost:3000${url}`, options);
+        const res = await fetch(`https://riddleprojectserver-1.onrender.com${url}`, options);
 
         if (res.status === 401) {
             return ('Token expired of invalid. Please login again.');
